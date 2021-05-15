8ee767273d7c61bcc9121712484fb0a7fe954e81
Sirozhiddin Zhumabaev <szhumabaev@fun.co>
2021-05-15 20:13:36 +0300

    web config name fix

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
     "system:properties",
-    "classpath:config/api.properties"
+    "classpath:config/web.properties"
 })
 public interface WebConfig extends Config {
 
